31097ea22827725174762983ade13d8b1f00a34e
pavan.gupta@algorisys.com <pavan.gupta@algorisys.com>
2022-03-09 10:54:04 +0530

    added features.md and kebab-format

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 import socket from "./socket.js"
-import "./chat_room"
+import "./chat-room"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
---
+++ features.md
@@ -0,0 +1,10 @@
+Features:-
+
+1. Guest can signs_up.
+2. Users can sign_in.
+3. Users visits to homepage.
+4. Users can see all the availables chat rooms.
+5. Users can visits any chat room.
+6. Users will see all the history of chats.
+7. Users can add new chats in room as authenticated user.
+8. Users can also add new chat room and also do chat in it.
